f95840ba95626443e10f447ff19afafb1bfcf201
Alexey Konotop <90930648+mrnetbear@users.noreply.github.com>
2024-09-11 23:08:05 +0300

    Add files via upload
    added Lagrange polynomial

--- nmTask1.cpp
+++ nmTask1.cpp
@@ -42,6 +42,7 @@ int interPoly(){
         }
     }
     //Print all coefficients
+    std::cout << "================================================================" << std::endl;
     for (int i=0; i<n; i++){
         for (int j = 0; j < n; j++)
         {
@@ -50,13 +51,15 @@ int interPoly(){
         std::cout << "| " << x(i,0) << " | " << f(i,0) << std::endl;
         
     }
+    std::cout << "================================================================" << std::endl;
     //Perform the transformation
     double det = A.Determinant();
     if (!det) return 1;
 
     aj = A.Invert(&det)*f;
 
-    //Print inverted coefficients
+    //Print inverted matrix
+    std::cout << "================Inverted matrix================" << std::endl;
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++)
         {
@@ -64,12 +67,27 @@ int interPoly(){
         }
         std::cout << std::endl;
     }
+    std::cout << "===============================================" << std::endl;
 
     //Print found coefficients
+    std::cout << "================Original coefficients================" << std::endl;
     for (int i = 0; i < n; i++){
         std::cout << "aj(" << i << ") = " << aj(i,0) << std::endl;
     }
+    std::cout << "=====================================================" << std::endl;
 
+    //Generate Lagrange coefficients
+    double lagrangeCoef[n];
+    for (int i = 0; i < n; i++) {
+        lagrangeCoef[i] = 1.0;
+        for (int j = 0; j < n; j++) {
+            if (j != i) {
+                lagrangeCoef[i] *= (x(i,0) - x(j,0));
+            }
+        }
+    }
+
+    //Generate original function
     double x1 [100];
     double fx1 [100];
     for (int i = 0; i < 100; i++){
@@ -77,6 +95,9 @@ int interPoly(){
         fx1[i] = sin(x1[i])*exp(-x1[i]);
     }
 
+    
+
+    //Generate approximated based Polynomial function 
     double x2 [100];
     double fx2 [100];
     for (int i = 0; i < 100; i++){
@@ -86,21 +107,47 @@ int interPoly(){
         }
     }
 
+    //Calculate Lagrange polynomial
+    double x3 [100];
+    double fx3 [100];
+    for (int i = 0; i < 100; i++){
+        x3[i] = 3.0/100.00 * i;
+        fx3[i] = 0.0;
+        for (int j = 0; j < n; j++){
+            double numerator = 1.0;
+            for (int k = 0; k < n; k++){
+                if (k != j) {
+                    numerator *= (x3[i] - x(k,0));
+                }
+            }
+            fx3[i] += (numerator / lagrangeCoef[j]) * f(j,0);
+        }
+    }
+
+    
+
     TGraph* origin = new TGraph (100, x1, fx1);
     origin->SetMarkerStyle(20);
     origin->SetMarkerColor(kRed);
     origin->SetLineColor(kRed);
     origin->SetLineWidth(2);
 
-    TGraph* approx = new TGraph (100, x2, fx2);
-    approx->SetMarkerStyle(20);
-    approx->SetMarkerColor(kBlue);
-    approx->SetLineColor(kBlue);
-    approx->SetLineWidth(2);
+    TGraph* approxPol = new TGraph (100, x2, fx2);
+    approxPol->SetMarkerStyle(30);
+    approxPol->SetMarkerColor(kBlue);
+    approxPol->SetLineColor(kBlue);
+    approxPol->SetLineWidth(4);
 
-    TLegend* legend = new TLegend(0.1,0.7,0.4,0.9);
+    TGraph* approxLag = new TGraph (100, x3, fx3);
+    approxLag->SetMarkerStyle(40);
+    approxLag->SetMarkerColor(kGreen);
+    approxLag->SetLineColor(kGreen);
+    approxLag->SetLineWidth(3);
+
+
     origin->Draw();
-    approx->Draw("same");
+    approxPol->Draw("same");
+    approxLag->Draw("same");
     
     return 0;
 }
